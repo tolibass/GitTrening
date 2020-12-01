4148ed30104f458ef2f5bfc7ff7e1d013d6449d1
anatoliy <tolybas@mail.ru>
2020-12-01 18:07:02 +0700

    testUITEXTFIELD1

--- GitTrenin/GitTrenin/ViewController.swift
+++ GitTrenin/GitTrenin/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var testText: UILabel!
-    
+    var x = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
