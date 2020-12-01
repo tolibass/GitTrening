bbab8983441570dccd4ca3d1eab9ef6a58097d74
anatoliy <tolybas@mail.ru>
2020-12-01 17:45:30 +0700

    test

---
+++ GitTrenin/GitTrenin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTrenin
+//
+//  Created by Anatoliy on 01.12.2020.
+//  Copyright © 2020 Mamchenko.Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
